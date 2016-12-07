4c67ab293aab5b43d0697269c2d04417c13d2348
Markus Englund <markus.s.englund@gmail.com>
2016-12-07 02:11:42 +0100

    configure for deployment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get("/:time", function(req, res) {
+
+    function unixInput(unix) {
+        var d = new Date(unix * 1000);
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        return months[d.getMonth()] +" "+ d.getDate() +", "+ d.getFullYear();
+    }
+    
+    
+    if (!isNaN(req.params.time)) {
+        var result = unixInput(req.params.time);
+        res.json({unix: req.params.time, natural: result});
+    } else{
+        var d = new Date(req.params.time);
+        if (!isNaN(d)) {
+            var unix = d/1000;
+            res.json({unix: unix, natural: req.params.time});
+        } else {
+            res.json({unix: null, natural:null});
+        }
+    }
 });
 
 module.exports = router;
